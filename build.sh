bf219b3f9aa8a6dfcb3180124d14487cb7e419be
Lok Jian Ming <jianminglok@hotmail.com>
2016-06-04 13:57:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 repo init -u https://github.com/CyanogenMod/android.git -b cm-12.1
-repo sync
+repo sync -q
 git clone https://github.com/jianminglok/android_device_xiaomi_hermes.git -b cm-12.1 device/xiaomi/hermes
 git clone https://github.com/jianminglok/android_device_xiaomi_mt6795-common.git -b cm-12.1 device/xiaomi/mt6795-common
 git clone https://github.com/jianminglok/android_vendor_xiaomi_hermes.git -b cm-12.1 vendor/xiaomi/hermes
