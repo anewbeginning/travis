bbb36653123262f5d4436c71f76a5713e6a477b5
anewbeginning <hereitbegins18@gmail.com>
2017-03-24 13:07:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 repo init -u https://github.com/nitrogen-project/android_manifest.git -b n2
 repo sync -q -j16
+git clone https://github.com/nitrogen-project/android_kernel_xiaomi_msm8956.git kernel/xiaomi/msm8956
